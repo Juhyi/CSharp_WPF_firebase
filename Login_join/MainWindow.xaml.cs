edcd6f7d281ff51535c4151e3a79b67a8206b753
Juhyi <dlwngml201@naver.com>
2024-07-02 17:52:39 +0900

    로그인테스트 파일 업로드

---
+++ Login_join/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using Google.Cloud.Firestore;
+using System.Collections;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FirestoreDocumentReference = Google.Cloud.Firestore.DocumentReference;
+
+namespace Login_join
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FirestoreDb db;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        // 로드 이벤트
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"test-325e6-firebase-adminsdk-rnle1-777f85439c.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+
+            db = FirestoreDb.Create("test-325e6");
+        }
+
+        // 회원가입 버튼 이벤트
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string id = textBox1.Text;
+            string pass = textBox2.Text;
+
+            if (id == "" || pass == "") //공백이 입력될 경우
+            {
+                MessageBox.Show("아이디 또는 비밀번호에 공백이 있습니다.");
+                return;
+            }
+            JoinManagement(id, pass);
+        }
+
+        // 로그인 버튼 이벤트
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string id = textBox1.Text;
+            string pass = textBox2.Text;
+
+            if (id == "" || pass == "") //공백이 입력될 경우
+            {
+                MessageBox.Show("아이디 또는 비밀번호에 공백이 있습니다.");
+                return;
+            }
+
+            LoginManagement(id, pass);
+        }
+
+        //회원가입 관리
+        private async void JoinManagement(string id, string pass)
+        {
+            bool idCheck = await FindId(id);
+            if (idCheck) { } //id가 이미 있으므로 회원가입 X
+            else if (!idCheck) //id가 없으므로 회원가입 O
+            {
+                Join(id, pass);
+                MessageBox.Show("회원가입이 완료되었습니다.");
+            }
+        }
+
+        //로그인 관리
+        private async void LoginManagement(string id, string pass)
+        {
+            bool idCheck = await FindId(id, pass);
+            if (idCheck) //id, pass 일치
+            {
+                MessageBox.Show("로그인 되었습니다.");
+            }
+            else if (!idCheck) //id, pass 일치하지 않음
+            {
+                MessageBox.Show("로그인에 실패하였습니다.");
+            }
+        }
+
+        //아이디 찾는 메서드
+        async Task<bool> FindId(string id)
+        {
+            Query qref = db.Collection("Join").WhereEqualTo("Id", id);
+            QuerySnapshot snap = await qref.GetSnapshotAsync();
+
+            foreach (DocumentSnapshot docsnap in snap)
+            {
+                if (docsnap.Exists)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //아이디, 비번 찾는 메서드
+        async Task<bool> FindId(string id, string pass)
+        {
+            Query qref = db.Collection("Join").WhereEqualTo("Id", id).WhereEqualTo("Pass", pass);
+            QuerySnapshot snap = await qref.GetSnapshotAsync();
+
+            foreach (DocumentSnapshot docsnap in snap)
+            {
+                if (docsnap.Exists)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //가입하는 메서드
+        void Join(string id, string pass)
+        {
+            FirestoreDocumentReference DOC = db.Collection("Join").Document();
+            Dictionary<string, object> data1 = new Dictionary<string, object>()
+            {
+                {"Id", id },
+                {"Pass", pass },
+            };
+            DOC.SetAsync(data1);
+        }
+
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using Google.Cloud.Firestore;
+using System.Collections;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FirestoreDocumentReference = Google.Cloud.Firestore.DocumentReference;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FirestoreDb db;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await Add_Document_with_AutoID();
+        }
+
+        async Task Add_Document_with_AutoID()
+        {
+            try
+            {
+                CollectionReference coll = db.Collection("Add_Document_With_AutoID");
+                Dictionary<string, object> data1 = new Dictionary<string, object>()
+                {
+                    {"FirstName", "Kim" },
+                    {"LastName", "inje" },
+                    {"PhoneNumber", "010-1234-5678" }
+                };
+                await coll.AddAsync(data1);
+                MessageBox.Show("Data added successfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error adding document: " + ex.Message);
+            }
+        }
+
+        async Task Add_Document_with_CustomID()
+        {
+            FirestoreDocumentReference DOC = db.Collection("SearchTest").Document("TestC");
+            Dictionary<string, object> data1 = new Dictionary<string, object>()
+            {
+                {"FirestName", "Moly" },
+                {"LastName","inje" },
+                {"PhoneNumber", "010-1234-5789" }
+            };
+            await DOC.SetAsync(data1);
+            MessageBox.Show("data added successfully");
+        }
+
+        async Task Add_Array()
+        {
+            FirestoreDocumentReference DOC = db.Collection("Add_Aray").Document("myDoc");
+            Dictionary<string, object> data1 = new Dictionary<string, object>();
+
+            ArrayList array = new ArrayList();
+            array.Add(123);
+            array.Add("name");
+            array.Add(true);
+
+            data1.Add("My Array", array);
+            await DOC.SetAsync(data1);
+
+            MessageBox.Show("data added successfully");
+        }
+
+        async Task Add_ListOfObjects()
+        {
+            FirestoreDocumentReference DOC = db.Collection("Add_ListOfObjects").Document("myDoc");
+            Dictionary<string, object> MainData = new Dictionary<string, object>();
+            Dictionary<string, object> List1 = new Dictionary<string, object>()
+            {
+                {"FirestName", "Kim" },
+                {"LastName","Jinwon" },
+                {"PhoneNumber", "010-1234-5678" }
+            };
+
+            MainData.Add("MyList", List1);
+
+            await DOC.SetAsync(MainData);
+            MessageBox.Show("data added successfully");
+        }
+
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory + @"test-325e6-firebase-adminsdk-rnle1-777f85439c.json";
+                Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+                db = FirestoreDb.Create("test-325e6");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error initializing Firestore: " + ex.Message);
+            }
+
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            await Add_Document_with_CustomID();
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            await Add_Array();
+        }
+
+        private async void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            await Add_ListOfObjects();
+        }
+    }
+}
